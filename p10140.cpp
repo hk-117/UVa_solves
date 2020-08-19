0e06bbff6dccdf215d429169c25d63c6df8ea4ca
hk_117 <55708454+hk-117@users.noreply.github.com>
2020-08-19 09:24:37 +0600

    Add files via upload

--- p10140.cpp
+++ p10140.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 typedef long long ll;
 ll sieve_size;
-bitset<10000010>C;
+bitset<20000010>C;
 vector<int> P;
+vector<pair<int,int>> PP;
 void sieve(ll n){
     sieve_size=n+1;
     C.set();
@@ -25,34 +26,20 @@ bool isPrime(ll N){
     }
     return true;
 }
+void twnPairs(){
+    ll a,b,i=3;
+    for(ll i=3;i<=20000000;i+=2){
+        if(isPrime(i) && isPrime(i+2)){
+            PP.push_back(make_pair(i,i+2));
+        }
+    }
+}
 int main(){
-    ll l,h;
-    sieve(10000000);
-    while(scanf("%lld %lld",&l,&h)==2){
-        vector<ll> Nprms;
-        for(ll i=l;i<=h;i++){
-            if(isPrime(i))
-                Nprms.push_back(i);
-        }
-        if(Nprms.size()<2){
-            puts("There are no adjacent primes.");
-        }
-        else{
-            ll mnl,mnh,mxl,mxh,mxdf=-1,mndf=1000001;
-            for(int i=0;i<Nprms.size()-1;i++){
-                ll diff= Nprms[i+1]-Nprms[i];
-                if(mxdf < diff){
-                    mxdf= diff;
-                    mxl=Nprms[i];
-                    mxh=Nprms[i+1];
-                }
-                if(mndf>diff){
-                    mndf=diff;
-                    mnl=Nprms[i];
-                    mnh=Nprms[i+1];
-                }
-            }
-            printf("%lld,%lld are closest, %lld,%lld are most distant.\n",mnl,mnh,mxl,mxh);
-        }
+    int n;
+    sieve(20000000);
+    twnPairs();
+    while(scanf("%d",&n)==1){
+        ll a= PP[n-1].first,b=PP[n-1].second;
+        printf("(%lld, %lld)\n",a,b);
     }
 }
