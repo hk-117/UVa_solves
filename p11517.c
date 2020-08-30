10ed6c779e80461b8ae7aa6bb7a51106fc954d8c
hk_117 <55708454+hk-117@users.noreply.github.com>
2020-08-30 22:15:58 +0600

    Update p11517.c

--- p11517.c
+++ p11517.c
@@ -5,7 +5,7 @@
 #define RI(i,a,b,c) for(i=a;i>=b;i-=c)
 #define min(a,b) (a)<(b)?a:b
 #define mx 105
-int mem[30001];
+int mem[15001];
 int main(){
     int t,n,v,C[105];
     scan(t);
@@ -16,15 +16,15 @@ int main(){
         R(i,0,n,1){
             scan(C[i]);
         }
-        R(i,0,30001,1) mem[i]=mx;
+        R(i,0,15001,1) mem[i]=mx;
         mem[0]=0;
         R(i,0,n,1){
-            RI(j,30001-C[i]-1,0,1){
+            RI(j,15001-C[i]-1,0,1){
                 if(mem[j] < mx)
                     mem[j+C[i]]=min(mem[j+C[i]],mem[j]+1);
             }
         }
-        R(i,v,30001,1){
+        R(i,v,15001,1){
             if(mem[i]!=mx){
                 printf("%d %d\n",i,mem[i]);
                 break;
