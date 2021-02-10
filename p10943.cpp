be127d5b4adab747f0ab9a7473817d36f8166845
hk_117 <55708454+hk-117@users.noreply.github.com>
2021-02-10 21:08:59 +0600

    Add files via upload

---
+++ p10943.cpp
@@ -0,0 +1,23 @@
+#include<cstdio>
+#include<cstring>
+#define MD 1000000
+using namespace std;
+int Table[105][105];
+int ways(int n,int k){
+  if(k==1){
+    return 1;
+  }
+  if(Table[n][k]!=-1) return Table[n][k];
+  int ans=0;
+  for(int i=0;i<=n;i++){
+    ans= (ans+ ways(n-i,k-1)) % MD;
+  }
+  return (Table[n][k]=ans);
+}
+int main(){
+  int N,K;
+  while(scanf("%d %d",&N,&K)==2 && (N||K)){
+    memset(Table,-1,sizeof Table);
+    printf("%d\n",ways(N,K));
+  }
+}
