e1569300529857a0c14648de86b714b1383913fb
hk_117 <55708454+hk-117@users.noreply.github.com>
2021-02-20 15:51:37 +0600

    Add files via upload

---
+++ p440.cpp
@@ -0,0 +1,18 @@
+#include<cstdio>
+using namespace std;
+int F(int n,int k){
+    if(n==1)
+        return 0;
+    else
+        return (F(n-1,k)+k)%n;
+}
+int main(){
+    int i,N;
+    while(scanf("%d",&N)==1 && N){
+        i=1;
+        while(F(N-1,i)+2 != 2){
+            i++;
+        }
+        printf("%d\n",i);
+    }
+}
